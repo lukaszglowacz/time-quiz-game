93e8779791896ef56e1f5fd713f97db0dc35fc01
Lukasz <bakatjur@gmail.com>
2023-02-28 15:19:51 +0000

    Add style to instructions button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // Home page popup window
 document.addEventListener('DOMContentLoaded', () => {
-    const myButton = document.querySelector('#myButton');
-    const popupContainer = document.querySelector('#popupContainer');
+    let myButton = document.querySelector('#myButton');
+    let popupContainer = document.querySelector('#popupContainer');
 
     myButton.addEventListener('click', () => {
         popupContainer.classList.add('show');
@@ -10,4 +10,17 @@ document.addEventListener('DOMContentLoaded', () => {
     popupContainer.addEventListener('click', () => {
         popupContainer.classList.remove('show');
     });
+});
+
+// Making a blur when click the read instructions button
+
+var hideButton = document.getElementById('myButton');
+hideButton.addEventListener('click', function() {
+  var boxes = document.querySelectorAll('.box');
+  for (var i = 0; i < boxes.length; i++) {
+    // use a condition to target only the first and third .box elements
+    if (i === 0 || i === 2) {
+      boxes[i].style.display = 'none';
+    }
+  }
 });
