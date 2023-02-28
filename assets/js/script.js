0621e6bf82eddd4155d562ff66d14e3204960e86
Lukasz <bakatjur@gmail.com>
2023-02-28 14:32:32 +0000

    Add instructions button to home page and add popup window with instructrions and style it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-console.log("Hello mate 23!");
+// Home page popup window
+document.addEventListener('DOMContentLoaded', () => {
+    const myButton = document.querySelector('#myButton');
+    const popupContainer = document.querySelector('#popupContainer');
+
+    myButton.addEventListener('click', () => {
+        popupContainer.classList.add('show');
+    });
+
+    popupContainer.addEventListener('click', () => {
+        popupContainer.classList.remove('show');
+    });
+});
