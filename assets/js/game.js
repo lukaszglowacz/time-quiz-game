cae04bbb078966f190466c91ecef7d8f4426b98d
Lukasz <bakatjur@gmail.com>
2023-03-01 09:59:49 +0000

    Add getNewQuestion() function to game page and create separate game js file

---
+++ assets/js/game.js
@@ -0,0 +1,56 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [{
+        question: "Do you know?",
+        choice1: "Yeah",
+        choice2: "Nope",
+        choice3: "What?",
+        choice4: "No",
+        answer: 3
+    },
+    {
+        question: "Do you feel",
+        choice1: "Yeah",
+        choice2: "Nope",
+        choice3: "What?",
+        choice4: "No",
+        answer: 1
+    },
+    {
+        question: "Do you learn?",
+        choice1: "Yeah",
+        choice2: "Nope",
+        choice3: "What?",
+        choice4: "No",
+        answer: 1
+    }
+];
+
+// Constans
+const CORRECT_BONUS = 10;
+const INCORRECT_FAILED = 20;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+};
+
+startGame();
