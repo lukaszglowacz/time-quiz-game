09646b0fa13f79f1dd550709bee3ad7d9af7aa91
Lukasz <bakatjur@gmail.com>
2023-03-01 09:12:00 +0000

    Create startGame() function

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,5 +15,52 @@ document.addEventListener('DOMContentLoaded', () => {
 // Game page
 
 const questionEl = document.getElementById("question");
-const choices = document.getElementsByClassName("choice-text");
-console.log(choices);
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+    {
+        question: "Do you know?",
+        choice1: "Yeah",
+        choice2: "Nope",
+        choice3: "What?",
+        choice4: "No",
+        answer: 3
+    },
+    {
+        question: "Do you feel",
+        choice1: "Yeah",
+        choice2: "Nope",
+        choice3: "What?",
+        choice4: "No",
+        answer: 1
+    },
+    {
+        question: "Do you learn?",
+        choice1: "Yeah",
+        choice2: "Nope",
+        choice3: "What?",
+        choice4: "No",
+        answer: 1
+    }
+];
+
+// Constans
+const CORRECT_BONUS = 10;
+const INCORRECT_FAILED = 20;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [ ... questions];
+    console.log(availableQuestions);
+};
+
+startGame();
