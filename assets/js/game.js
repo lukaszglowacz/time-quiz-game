4b3b1bf715e70d235234549e6e9ce7ac91fadfba
Lukasz <bakatjur@gmail.com>
2023-03-01 10:44:02 +0000

    Add startTimer function which allows to start the stopwatch when player start a game

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,6 +8,7 @@ let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
+
 let questions = [{
         question: "Do you know?",
         choice1: "Yeah",
@@ -39,24 +40,28 @@ const CORRECT_BONUS = 10;
 const INCORRECT_FAILED = 20;
 const MAX_QUESTIONS = 3;
 
+let elapsedTime = 0;
+
 startGame = () => {
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
     getNewQuestion();
+    // start the timer
+    startTimer();
 };
 
 getNewQuestion = () => {
-        if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-            // go to the end page
-            return window.location.assign('/index.html');
-        }
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        // go to the end page
+        return window.location.assign('/index.html');
+    }
     questionCounter++;
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
 
-    choices.forEach (choice => {
+    choices.forEach(choice => {
         const number = choice.dataset['number'];
         choice.innerText = currentQuestion['choice' + number];
     });
@@ -67,7 +72,7 @@ getNewQuestion = () => {
 
 choices.forEach(choice => {
     choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return;
+        if (!acceptingAnswers) return;
 
         acceptingAnswers = false
         const selectedChoice = e.target;
@@ -75,5 +80,22 @@ choices.forEach(choice => {
         getNewQuestion();
     });
 });
+
+//stopwatch function
+
+
+
+startTimer = () => {
+    const timerElement = document.getElementById("timer");
+    let minutes = 0;
+    let seconds = 0;
+    timer = setInterval(() => {
+        elapsedTime++;
+        minutes = Math.floor(elapsedTime / 60);
+        seconds = elapsedTime % 60;
+        timerElement.innerText = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    }, 1000);
+};
+
 
 startGame();
